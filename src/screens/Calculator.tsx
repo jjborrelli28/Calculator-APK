bc80b552a78f352ee9f8eb8065edae9af6561dd4
Juan <juanjoseborrelli92@outlook.com>
2021-11-04 20:42:23 -0300

    Structuring

--- src/screens/Calculator.tsx
+++ src/screens/Calculator.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
 import {styles} from './theme/styles';
 
 const Calculator = () => {
-  return <SafeAreaView style={styles.base}></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.base}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <View style={styles.calculatorContainer}>
+        <Text style={styles.smallResult}>1,500</Text>
+        <Text style={styles.result}>1,500</Text>
+        <View>
+          <View>{/* Btn */}</View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Calculator;
--- src/screens/theme/styles.tsx
+++ src/screens/theme/styles.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
@@ -6,5 +5,24 @@ export const styles = StyleSheet.create({
     backgroundColor: 'black',
     flex: 1,
     color: 'white',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  },
+  calculatorContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
+  result: {
+    color: 'white',
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  smallResult: {
+    color: 'rgba(255,255,255,.5)',
+    fontSize: 30,
+    textAlign: 'right',
   },
 });
