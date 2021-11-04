3951611b206cc757344f999e6d6782ec17661c6f
Juan <juanjoseborrelli92@outlook.com>
2021-11-04 20:21:29 -0300

    Starting app

---
+++ src/screens/Calculator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import {styles} from './theme/styles';
+
+const Calculator = () => {
+  return <SafeAreaView style={styles.base}></SafeAreaView>;
+};
+
+export default Calculator;
